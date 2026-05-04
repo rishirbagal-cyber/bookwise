e3e37933b022dc397d660483c0d8eefe25ada3d6
rishirbagal-cyber <rishirbagal@gmail.com>
2026-05-04 10:27:25 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Bookwise API is running." });
+});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("Successfully connected to MongoDB Atlas");
+
+  app.listen(process.env.PORT || 10000, () => {
+    console.log(`Server running on port ${process.env.PORT || 10000}`);
+  });
+})
+.catch((err) => console.error(err));
